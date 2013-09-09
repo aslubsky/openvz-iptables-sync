6999de64333f9527d0de07e701e84d7dc2b3938d
Alex Slubsky <aslubsky@gmail.com>
2013-09-09 20:53:00 +0300

    add protocol select

--- iptables.php
+++ iptables.php
@@ -21,14 +21,14 @@ foreach($portsFwd as $portFwd) {
     }
     $str []= sprintf(
         '/sbin/iptables -t nat -A PREROUTING -p %s -d %s --dport %s -i %s -j DNAT --to-destination %s:%s',
-        $portFwd['proto'], $wanIP, $portFwd['port_from'], $wanIF, $portFwd['port_to'], $portFwd['ip_address']
+        $portFwd['proto'], $wanIP, $portFwd['port_from'], $wanIF, $portFwd['ip_address'], $portFwd['port_to']
     );
     $identity = $portFwd['node_id'];
 }
 file_put_contents(SITE_DIR.'/all', implode("\n", $str));
 
-shell_exec(SITE_DIR.'/main');
+// shell_exec(SITE_DIR.'/main');
 shell_exec('chmod 755 '.SITE_DIR.'/all');
-shell_exec(SITE_DIR.'/all');
+// shell_exec(SITE_DIR.'/all');
 
 exit('OK');
